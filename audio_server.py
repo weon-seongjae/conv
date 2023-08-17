86aeab4336de78e86dc16a34b47a171173b939c0
weonsj <weonsj@gmail.com>
2023-08-17 15:42:23 +0900

    add main_app4_multi_audio.py

--- audio_server.py
+++ audio_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, send_from_directory
 from flask_cors import CORS
 import os
+import time
 
 app = Flask(__name__, static_url_path='/static', static_folder='.')
 CORS(app)
@@ -12,6 +13,7 @@ if not os.path.exists(audio_directory):
 
 @app.route('/audio/<path:filename>', methods=['GET'])
 def download(filename):
+    print(f"Sending audio file {filename}")
     return send_from_directory(audio_directory, filename)
 
 @app.route('/delete/audio/<path:filename>', methods=['POST'])
@@ -19,8 +21,10 @@ def delete_file(filename):
     file_path = os.path.join(audio_directory, filename)
     try:
         os.remove(file_path)
+        print(f"Deleted audio file {filename}")
         return Response(status=204)  # 204 No Content, 성공적인 삭제
     except FileNotFoundError:
+        print(f"File not found {filename}")
         return Response(status=404)  # 파일이 없으면 404 Not Found
 
 if __name__ == "__main__":
